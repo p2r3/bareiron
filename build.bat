2802a6042f20e83cee97fe3d8a5ee0fcc7dcdd16
zyriu1 <Suvkq@hotmail.com>
2025-09-12 12:46:29 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
+setlocal enabledelayedexpansion
+set files=
+for /f "usebackq delims=" %%f in (`dir /b "%cd%\src\*.c"`) do (
+    set files=!files! src\%%f
+)
 if exist bareiron.exe del bareiron.exe
-gcc src
+gcc %files% -o bareiron
+pause
