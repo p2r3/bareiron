f12fa71af7efea434fc8d595e5689d17241849c5
Max Parry <66670928+Buggem@users.noreply.github.com>
2025-09-14 10:18:00 +1000

    Add node: prefixes to build_registries script

--- build_registries.js
+++ build_registries.js
@@ -1,5 +1,5 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
 // Overrides for block-to-item conversion
 const blockToItemOverrides = {
