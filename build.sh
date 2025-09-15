a03d3e70385d36932e8a924fc8208ff842264cea
netceil <51170759+netceil@users.noreply.github.com>
2025-09-15 18:50:18 +0200

    -march=native

--- build.sh
+++ build.sh
@@ -41,5 +41,5 @@ for arg in "$@"; do
 done
 
 rm -f "bareiron$exe"
-$compiler src/*.c -O3 -Iinclude -o "bareiron$exe" $windows_linker
+$compiler src/*.c -O3 -march=native -Iinclude -o "bareiron$exe" $windows_linker
 "./bareiron$exe"
